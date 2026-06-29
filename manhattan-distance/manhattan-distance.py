e41b1e8bc4d9b7ab9be94a86487cff80f24ed8c7
Vikram Rangarajan <101434931+VikramRangarajan@users.noreply.github.com>
2026-06-29 17:50:06 +0530

    Solve manhattan-distance

---
+++ manhattan-distance/manhattan-distance.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def manhattan_distance(x, y):
+    """
+    Compute the Manhattan (L1) distance between vectors x and y.
+    Must return a float.
+    """
+    # Write code here
+    return np.abs(np.subtract(x, y)).sum().item()
